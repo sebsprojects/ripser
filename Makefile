2c7cdc4bdc8d6c54635ff043c2006435228c857e
Sebastian Kreisel <seb@elfeck.com>
2021-05-07 14:55:51 +0200

    Fix: Small isse in cohom_clearing and added cohom_emergent

--- Makefile
+++ Makefile
@@ -9,5 +9,11 @@ ripser_hom: ripser_hom.cpp
 ripser_cohom_clearing: ripser_cohom_clearing.cpp
 	c++ -std=c++11 -Wall ripser_cohom_clearing.cpp -o ripser_cohom_clearing -g
 
+ripser_hom_clearing: ripser_hom_clearing.cpp
+	c++ -std=c++11 -Wall ripser_hom_clearing.cpp -o ripser_hom_clearing -g
+
+ripser_cohom_emergent: ripser_cohom_emergent.cpp
+	c++ -std=c++11 -Wall ripser_cohom_emergent.cpp -o ripser_cohom_emergent -g
+
 clean:
-	rm -f ripser_cohom ripser_hom ripser_cohom_clearing
+	rm -f ripser_cohom ripser_hom ripser_cohom_clearing ripser_hom_clearing ripser_cohom_emergent
--- ripser_cohom_emergent.cpp
+++ ripser_cohom_emergent.cpp
@@ -14,15 +14,39 @@ typedef std::priority_queue<index_diameter_t,
 index_diameter_t init_coboundary_and_get_pivot(ripser &ripser,
                                                const index_diameter_t simplex,
                                                const index_t dim,
-                                               Column& working_coboundary) {
+                                               Column& working_coboundary,
+                                               entry_hash_map& pivot_column_index) {
 	simplex_coboundary_enumerator cofacets(ripser);
 	cofacets.set_simplex(simplex, dim);
+	bool check_for_emergent_pair = true;
+	// This is neccessary since we want to clear the working_coboundary
+	// priority_queue in case we find a emergent pair, but prioirty_queue
+	// does not provide a clear method. Thus we add the elements to a buffer first
+	// and if no emergent pair is detected only then do we write to
+	// working_coboundary
+	// TODO: Find a more efficient solution to this annoying problem
+	std::vector<index_diameter_t> working_coboundary_buffer;
 	while(cofacets.has_next()) {
 		index_diameter_t cofacet = cofacets.next();
 		// Threshold check
 		if(get_diameter(cofacet) <= ripser.threshold) {
-			working_coboundary.push(cofacet);
+			working_coboundary_buffer.push_back(cofacet);
+			// Emergent pair candidate check
+			if(check_for_emergent_pair &&
+			   (get_diameter(simplex) == get_diameter(cofacet))) {
+				// Check if the candidate is viable, if not then we certainly
+				// do not have an emergent pair
+				if((pivot_column_index.find(get_index(cofacet)) ==
+				    pivot_column_index.end())) {
+					// working_coboundary is the 0-column
+					return cofacet;
+				}
+				check_for_emergent_pair = false;
+			}
 		}
+	}
+	for(index_diameter_t cofacet : working_coboundary_buffer) {
+		working_coboundary.push(cofacet);
 	}
 	return get_pivot(working_coboundary);
 }
@@ -31,13 +55,9 @@ index_diameter_t init_coboundary_and_get_pivot(ripser &ripser,
 // to contain all cofacets of those simplices
 // ordered column indices (indexed by dim-simplices) of the coboundary matrix
 // Sets simplices to contain all dim-simplices
-//
-// Clearing: We take the pivots from the reduction in dim-1 and ignore columns
-// that appeared as a pivot
 void assemble_columns_to_reduce(ripser &ripser,
                                 std::vector<index_diameter_t>& simplices,
                                 std::vector<index_diameter_t>& columns_to_reduce,
-                                entry_hash_map& pivot_column_index,
                                 const index_t dim) {
 	columns_to_reduce.clear();
 	std::vector<index_diameter_t> next_simplices;
@@ -47,13 +67,9 @@ void assemble_columns_to_reduce(ripser &ripser,
 		while(cofacets.has_next(false)) {
 			index_diameter_t cofacet = cofacets.next();
 			// Threshold check
-			next_simplices.push_back(cofacet);
 			if(get_diameter(cofacet) <= ripser.threshold) {
-				// Clearing check
-				if(pivot_column_index.find(get_index(cofacet)) ==
-				   pivot_column_index.end()) {
-					columns_to_reduce.push_back(cofacet);
-				}
+				next_simplices.push_back(cofacet);
+				columns_to_reduce.push_back(cofacet);
 			}
 		}
 	}
@@ -65,6 +81,7 @@ void assemble_columns_to_reduce(ripser &ripser,
 void compute_pairs(ripser &ripser,
                    const std::vector<index_diameter_t>& columns_to_reduce,
                    entry_hash_map& pivot_column_index,
+                   const entry_hash_map& previous_pivots,
                    const index_t dim) {
 	std::cout << "persistence intervals in dim " << dim << ":" << std::endl;
 	compressed_sparse_matrix reduction_matrix; // V
@@ -78,7 +95,8 @@ void compute_pairs(ripser &ripser,
 		index_diameter_t pivot = init_coboundary_and_get_pivot(ripser,
 		                                                       column_to_reduce,
 		                                                       dim,
-		                                                       working_coboundary);
+		                                                       working_coboundary,
+		                                                       pivot_column_index);
 		// The reduction
 		// The loop terminates on either of two conditions
 		//   1. R_j got reduced to a zero column with pivot index -1 (the loop condiiton)
@@ -119,10 +137,13 @@ void compute_pairs(ripser &ripser,
 			}
 		} else {
 			// Zero column
-			// Since we use clearing, zero columns that correspond to killing
-			// simplices are filtered out by assemble_columns_to_reduce
-			// Thus all zero columns are birth indices of an essential pair
-			std::cout << " [" << birth << ", )" << std::endl;
+			auto pair = previous_pivots.find(get_index(column_to_reduce));
+			if(pair == previous_pivots.end()) {
+				// Essential index!
+				std::cout << " [" << birth << ", )" << std::endl;
+			} else {
+				// Killing index non-essential pair
+			}
 		}
 	}
 }
@@ -130,6 +151,7 @@ void compute_pairs(ripser &ripser,
 void compute_barcodes(ripser& ripser) {
 	std::vector<index_diameter_t> simplices;
 	entry_hash_map pivot_column_index;
+	entry_hash_map previous_pivots;
 	for(index_t i = 0; i < ripser.n; i++) {
 		value_t diam = ripser.compute_diameter(i, 0);
 		simplices.push_back(index_diameter_t(i, diam));
@@ -139,13 +161,13 @@ void compute_barcodes(ripser& ripser) {
 		if(dim == 0) {
 			columns_to_reduce = std::vector<index_diameter_t>(simplices);
 		} else {
-			// Takes the pivots from the previous iteration
-			assemble_columns_to_reduce(ripser, simplices, columns_to_reduce,
-			                           pivot_column_index, dim);
+			assemble_columns_to_reduce(ripser, simplices, columns_to_reduce, dim);
 		}
 		pivot_column_index.clear();
 		pivot_column_index.reserve(columns_to_reduce.size());
-		compute_pairs(ripser, columns_to_reduce, pivot_column_index, dim);
+		compute_pairs(ripser, columns_to_reduce, pivot_column_index, previous_pivots, dim);
+		//TODO(seb): reserve enough space in previous_pivots?
+		previous_pivots.swap(pivot_column_index);
 	}
 }
 
