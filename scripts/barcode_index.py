6a2c866706382daee793d5c28683b4a68f307a08
Sebastian Kreisel <seb@elfeck.com>
2021-11-13 21:18:12 +0100

    Add: Better barcode plot scripts

--- scripts/barcode_index.py
+++ scripts/barcode_index.py
@@ -7,13 +7,10 @@ import numpy as np
 import math
 import sys
 from functools import cmp_to_key
-from matplotlib.ticker import FormatStrFormatter
-
 
 if len(sys.argv) < 1:
     print("error: missing config file-path")
     sys.exit()
-
 
 # PARAMETER
 # -----------------------------------------------------------------------------
@@ -40,7 +37,7 @@ tex_fonts = {
     'text.latex.preamble': r'\usepackage{amsfonts}'
 }
 plt.rcParams.update(tex_fonts)
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(figw, figw))
 
 
 # FUNCTION DEFS
@@ -54,72 +51,77 @@ def comp_bar(a, b):
     else:
         return 1
 
-def read_barcode():
+def read_barcode(simplex_lookup):
     f = open(input_file, "r");
     intervals = []
     max_bound = 0
-    bounds = []
+    offs = 1
     for line in f:
         if line.startswith("#b"):
             dim = int(line[2])
             if dim + 1 > len(intervals):
                 intervals.append([dim,[]])
             toks = [t.strip() for t in line[3:].split("-")]
-            sint = toks[0].replace("[","").replace(")", "").split(",")
-            if sint[0] == sint[1]:
-                continue
-            iint = [float(sint[0])]
-            bounds.append(float(sint[0]))
+            sint = toks[1].replace("[","").replace(")", "").split(",")
+            start_index = int(sint[0])
+            iint = [(simplex_lookup.index([dim, start_index])) + offs]
             if sint[1].strip() != "":
-                iint.append(float(sint[1]))
-                bounds.append(float(sint[1]))
+                end_index = int(sint[1])
+                iint.append(simplex_lookup.index([dim+1, end_index]) + offs)
                 max_bound = max(max_bound, iint[1])
             max_bound = max(max_bound, iint[0])
             intervals[dim][1].append(iint)
     for dim, b in enumerate(intervals):
         for i, interval in enumerate(b[1]):
             if len(interval) == 1:
-                intervals[dim][1][i].append(max_bound * 1.1)
+                intervals[dim][1][i].append(max_bound + 1)
         intervals[dim][1] = sorted(intervals[dim][1], key=cmp_to_key(comp_bar))
-    return (sorted(list(set(bounds))), intervals)
+    return (max_bound, intervals)
 
-def plot_barcode(bounds, intervals):
-    h_inc = (bounds[-1] * 1.1) * 0.02
+def read_simplices():
+    f = open(input_file, "r")
+    simplex_lookup = []
+    for line in f:
+        if line.startswith("#s"):
+            dim = int(line[2])
+            index = int(line[3:].split("-")[0].strip())
+            simplex_lookup.append([dim, index])
+    return simplex_lookup
+
+def plot_barcode(max_bound, intervals):
+    h_inc = (max_bound + 1) * 0.02
     h_skip = h_inc * 1.5
     h = 0
-    ax.set_xlim(0, bounds[-1] * 1.1)
-    ax.set_xticks(bounds)
+    ax.set_xlim(1, max_bound+1)
+    ax.set_xticks(range(1, max_bound+1))
     ax.xaxis.grid(True, linestyle="dotted")
     ax.set_yticks([])
     ax.spines["left"].set_visible(False)
     ax.spines["right"].set_visible(False)
-    ax.xaxis.set_major_formatter(FormatStrFormatter('%.3g'))
     for bc in intervals:
         dim = bc[0]
         bars = bc[1]
-        if len(bars) == 0:
-            continue
         h += h_skip
         for b in bars:
-            print(b, h)
             ax.plot(b, [h, h], color=colors[dim])
             if b != bars[-1]:
                 h += h_inc
     h += h_skip
     ax.set_ylim(0,h)
+    ax.set_aspect("equal")
     #ax2 = ax.twiny()
     #ax2.set_xlim(ax.get_xlim())
-    #ax2.set_xticks(bounds)
+    #ax2.set_xticks(ax.get_xticks())
     #ax2.spines["right"].set_visible(False)
     #ax2.spines["left"].set_visible(False)
-    ax.set_aspect("equal")
-    fig.set_figwidth(figw)
+    fig.set_figheight(h * 0.5)
 
 
 # MAIN
 # -----------------------------------------------------------------------------
 
-bounds, intervals = read_barcode()
-plot_barcode(bounds, intervals)
-plt.savefig("../thesis/img/test1.pdf", format='pdf', bbox_inches="tight", pad_inches=0.05)
+simplex_lookup = read_simplices()
+max_bound, intervals = read_barcode(simplex_lookup)
+plot_barcode(max_bound, intervals)
+plt.savefig("../thesis/img/test.pdf", format='pdf', bbox_inches="tight", pad_inches=0.05)
 #plt.show()
